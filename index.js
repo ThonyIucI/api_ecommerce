67bf15a32116676652d0b1ecb913208399721b19
ThonyIucI <anthonyau011798@gmail.com>
2022-07-17 23:27:32 -0500

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { lock } = require("./src/app.js");
 const app = require("./src/app.js");
 const { sequelize } = require("./src/db.js");
 
-const PORT_B = process.env.PORT || 3001;
+const PORT_B = process.env.PORT;
 
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT_B, () => {
